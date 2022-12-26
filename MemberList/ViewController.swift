e949ded4bf43f10b78404c1eda1fad62f5d8e52a
zsuant <lsg001025@gmail.com>
2022-12-26 23:59:34 +0900

    Initial Commit

---
+++ MemberList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MemberList
+//
+//  Created by 이수겸 on 2022/12/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
